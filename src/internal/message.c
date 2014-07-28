31c64ce0000e6f8a6c6031855e09b5653da02c52
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-07-28 08:44:21 -0400

    Refactored message.c
    Removed a small amount of duplicate code.

--- src/internal/message.c
+++ src/internal/message.c
@@ -44,45 +44,19 @@ static FILE * getConversationFileInMode(const char * userA, const char * userB, 
 
 	char buffer[(BUFFER_LENGTH*3) + strlen(DATA_DIR)]; //*3 for safety
 	bzero(buffer, sizeof(buffer));
-	if(order < 1){
-		snprintf(buffer, sizeof(buffer), "%s%s-%s", DATA_DIR, userA, userB);
-	}else{
-		snprintf(buffer, sizeof(buffer), "%s%s-%s", DATA_DIR, userB, userA);
-	}
+	getConversationFileName(userA, userB, buffer);
 
-	int existsAlready = directory_exists(buffer);
-	if(existsAlready != 1){
-		fprintf(stderr, "%s%s\n", FAILED_DIR_EXISTS, buffer);
+	if(strlen(buffer) <= 0){
+		fprintf(stderr, "%s\n", "Failed to compute filename");
 		return NULL;
 	}
 
-		/* Conversation Directory Exists, Order conversations by time */
-	char timeBuffer[BUFFER_LENGTH];
-	bzero(timeBuffer, sizeof(timeBuffer));
-
-	/* pretty much exactly from strftime's man page: */
-	time_t t;
-	struct tm * tmp;
-	t = time(NULL);
-	tmp = localtime(&t);
-	if(tmp == NULL){
-		fprintf(stderr, "Failed to determine local time\n");
-		return 0;
-	}
-	strftime(timeBuffer, sizeof(timeBuffer), "/%F", tmp);
-
-	if(strlen(timeBuffer) + strlen(buffer) + 1 > sizeof(buffer)){
-		fprintf(stderr, "Filename too large for conversation between %s and %s\n:%s/%s\n", userA, userB, buffer, timeBuffer);
+	if(file_exists(buffer) == 0){
+		fprintf(stderr, "%s %s\n", FAILED_FILE_EXISTS, buffer);
 		return NULL;
 	}
 
-	char * fileName = strncat(buffer, timeBuffer, sizeof(buffer));
-	if(file_exists(fileName) == 0){
-		fprintf(stderr, "%s %s\n", FAILED_FILE_EXISTS, fileName);
-		return NULL;
-	}
-
-	return fopen(fileName, mode);
+	return fopen(buffer, mode);
 }
 
 
@@ -94,7 +68,7 @@ FILE * getConversationFileForWriting(const char * userA, const char * userB){
 	return getConversationFileInMode(userA, userB, "a");
 }
 
-
+/* Note createConversation does not use getConversationFileName since it must create directories along the way */
 int createConversation(const char * userA, const char * userB){
 	int order = strncmp(userA,userB,BUFFER_LENGTH-1);
 	if(order == 0) return 0; //You can't talk to yourself.
@@ -151,6 +125,7 @@ int createConversation(const char * userA, const char * userB){
 		fprintf(stderr, "%s %s\n", FAILED_FILE_CREATION, fileName);
 		return 0;
 	}
+	fflush(fp);
 	fclose(fp);
 	return 1;
 } 
