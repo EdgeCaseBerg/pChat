5bbb9a29d6e82ecb9795e4022970a3ddbf3cd0ce
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-07-29 08:33:32 -0400

    Ajax Form Submission for better chat.

--- public/js/talk.js
+++ public/js/talk.js
@@ -43,4 +43,30 @@ jQuery( document ).ready(function( $ ) {
 		}
 		timeout = setTimeout(loadConversation, period)
 	})
+
+	/* In order to facilate a more user friendly interface, submit the forms
+	 * via AJAX.
+	 */
+	 $('form').submit(function(evt){
+	 	evt.preventDefault()
+	 	var data = $(this).serialize()
+	 	var url = $(this).attr('action')
+	 	var method = $(this).attr('method')
+	 	$(this).fadeOut()
+	 	$.ajax({
+	 		type: method,
+	 		beforeSend: function(xhr){
+				xhr.withCredentials = true
+			},
+			data: data,
+			url: url,
+			context: this,
+			success: function(response){
+				console.info("Submitted Form for chat")
+				$(this).find('textarea').val("")
+				$(this).fadeIn()
+			}
+	 	})
+	 	return false
+	 })
 })
